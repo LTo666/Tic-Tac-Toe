a5caeb180ecdea00a08c92e09416f166e0b08693
Bruce Luk <lukusto@gmail.com>
2022-06-23 02:59:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,125 @@
+$(function () {
+  let fields = $(".field")
+  let clicks = 0
+  let crosses = []
+  let circles = []
+  let isWin = false
+  let turnsElem = $(".turns")
+  let isEndGame = false
+
+  let possibleCombinations = [
+    "147",
+    "258",
+    "369",
+    "123",
+    "456",
+    "789",
+    "159",
+    "357",
+  ]
+
+  fields.on("click", function (e) {
+    let field = $(`#${this.id}`)
+    clicks++
+    if (field.hasClass("cross") || field.hasClass("circle")) {
+      return
+    }
+    if (clicks % 2 === 0) {
+      if (isWin === true) {
+        return
+      } else {
+        $('.slider').css("background-color", "red")
+        turnsElem.removeClass("swc")
+        field.addClass("circle")
+        circles.push(`${this.id}`)
+        let compare = circles.sort((a, b) => {
+          return a - b
+        })
+        if (circles.length == 3) {
+          let strCom = compare.join("")
+          checkWin(strCom, "Circle")
+        } else if (circles.length == 4) {
+          checkWin(`${compare[0]}${compare[1]}${compare[2]}`, "Circle")
+          checkWin(`${compare[0]}${compare[1]}${compare[3]}`, "Circle")
+          checkWin(`${compare[0]}${compare[2]}${compare[3]}`, "Circle")
+          checkWin(`${compare[1]}${compare[2]}${compare[3]}`, "Circle")
+        }
+      }
+    } else if (clicks % 2 !== 0) {
+      if (isWin === true) {
+        return
+      } else {
+        $('.slider').css("background-color", "#edc760")
+        turnsElem.addClass("swc")
+        field.addClass("cross")
+        crosses.push(`${this.id}`)
+        let compare = crosses.sort((a, b) => {
+          return a - b
+        })
+        if (compare.length === 3) {
+          let strCom = compare.join("")
+          checkWin(strCom, "Cross")
+        } else if (compare.length === 4) {
+          checkWin(`${compare[0]}${compare[1]}${compare[2]}`, "Cross")
+          checkWin(`${compare[0]}${compare[1]}${compare[3]}`, "Cross")
+          checkWin(`${compare[0]}${compare[2]}${compare[3]}`, "Cross")
+          checkWin(`${compare[1]}${compare[2]}${compare[3]}`, "Cross")
+        } else if (compare.length === 5) {
+          checkWin(`${compare[0]}${compare[1]}${compare[2]}`, "Cross")
+          checkWin(`${compare[0]}${compare[1]}${compare[3]}`, "Cross")
+          checkWin(`${compare[0]}${compare[2]}${compare[3]}`, "Cross")
+          checkWin(`${compare[1]}${compare[2]}${compare[3]}`, "Cross")
+          checkWin(`${compare[1]}${compare[2]}${compare[4]}`, "Cross")
+          checkWin(`${compare[1]}${compare[3]}${compare[4]}`, "Cross")
+          checkWin(`${compare[2]}${compare[3]}${compare[4]}`, "Cross")
+          if (isEndGame) {
+            return
+          } else if (isEndGame === false && isWin === false) {
+            $('.msg').css('color', '#566fed')
+            $('.msg').html(`Draw!`)
+            $('.result').addClass('show')
+            isEndGame = true
+          }
+        }
+      }
+    } else {
+      return
+    }
+  })
+
+  const checkWin = (arr, symbol) => {
+    if (isEndGame) {
+      return
+    }
+    possibleCombinations.includes(arr) ? (isWin = true) : (isWin = false)
+    if (isWin) {
+      if (symbol === "Cross") {
+        $('.msg').css('color', 'red')
+      } else {
+        $('.msg').css('color', '#edc760')
+      }
+      $('.msg').html(`${symbol} wins!`)
+      $('.result').addClass('show')
+      isEndGame = true
+    }
+  }
+
+  $("#reload-btn").click(function () { restart() })
+
+  $('#play-again-btn').click(function () {
+     restart() 
+     $('.result').removeClass('show')
+  })
+
+  const restart = () => {
+    $(".circle").removeClass("circle")
+    $(".cross").removeClass("cross")
+    $('.slider').css("background-color", "red")
+    $('.swc').removeClass('swc')
+    clicks = 0
+    crosses = []
+    circles = []
+    isWin = false
+    isEndGame = false
+  }
+})
